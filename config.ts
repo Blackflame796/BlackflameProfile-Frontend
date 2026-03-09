97bbb440f6a3c591176b4d997423bd48a966914a
Blackflame796 <pws15548@gmail.com>
2026-03-10 00:07:56 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 export const DEBUG = false;
-export const BASE_URL = DEBUG ? "http://localhost:8000" : "https://blackflame.vercel.app";
+export const BASE_URL = DEBUG ? "http://localhost:8000" : "https://dondegusto.ru:5443/api";
