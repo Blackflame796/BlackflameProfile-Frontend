b16abdab6ac42076c9bc52332cf4c31908315b3b
Blackflame796 <pws15548@gmail.com>
2026-03-14 00:32:05 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 export const DEBUG = false;
-export const BASE_URL = DEBUG ? "http://localhost:8000" : "https://dondegusto.ru:5443/api";
+export const BASE_URL = DEBUG ? "http://localhost:8000" : "https://dondegusto.ru:5080";
